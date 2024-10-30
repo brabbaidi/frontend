8efea1ae51cfffd201105235c94d84f5e7cc7531
brabbaidi <brabbaidi@gmail.com>
2024-10-30 12:39:19 -0400

    Started the Frontend README file

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-# React + Vite
+# Resources
+ - Class Material
+ - Youtube Video: https://www.youtube.com/watch?v=P43DW3HUUH8&t=22s
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Challenges
+- Postman/MongoDB (404 error messages)
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-Inside the frontend/src folder, we have the following structure:
 
--components/
-    Navbar.js
--pages/
-    Home.js
-    Login.js
-    Register.js
-    ProductList.js
-    ProductDetails.js
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import CreatePage from './pages/CreatePage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
 
 const App = () => (
-    <Router>
-        <Navbar />
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/create" element={<CreatePage />} />
-        </Routes>
-    </Router>
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/create" element={<CreatePage />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
